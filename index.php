24d95ce50a37e235c820ec1e57f66082d97b9297
Chameera Lakshitha <chameera.lakshitha212@gmail.com>
2017-01-13 12:28:53 +0530

    check database

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
 
 echo 'Hello World';
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 ?>
